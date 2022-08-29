6d63966f724d6e1a8f9b05df64a4f335f87abca9
unknown <dgoodenough@proton.me>
2022-08-29 16:42:44 -0700

    lay out gameboarArray and how that displays. Stuck on how to define input

---
+++ tictactoe.js
@@ -0,0 +1,79 @@
+let boardDisplay = "123\n456\n789 ";
+let boardArray = [
+    [null, null, 'X'],
+    [null, null, null],
+    [null, null, null]
+]
+
+
+function defineInput(userInput) {
+  switch(userInput){
+    case 1:
+        userInput = boardArray[0][0];
+        break;
+    case 2:
+        userInput = boardArray[0][1];
+        break;
+    case 3:
+        userInput = boardArray[0][2];
+        break;
+
+    case 4:
+        userInput = boardArray[1][0];
+        break;
+    case 5:
+        userInput = boardArray[1][1];
+        break;
+    case 6:
+        userInput = boardArray[1][2];
+        break;
+    case 7:
+        userInput = boardArray[2][0];
+        break;
+    case 8:
+        userInput = boardArray[2][1];
+        break;
+    case 9:
+        userInput = boardArray[2][2];
+        break;
+  }
+  return userInput;
+}
+
+
+//define moves as a position in board array
+
+function getUserInput() {
+    let userInput = Number(prompt('Please make a move'));
+    console.log(typeof(userInput))
+
+    if(userInput !== 'number') {
+        alert('Invalid');
+        getUserInput();
+    }
+    userInput = defineInput(userInput);
+    console.log(userInput)
+
+    //check is hmove valid
+      //is empty square
+    //return input
+}
+getUserInput();
+
+//check if game over
+  //check if currentPlayer won
+  //check if board is full
+//otherwise update board
+// swith current player
+// return to get user input
+
+function updateBoard() {
+
+}
+
+//check is move valid
+
+function tictactoe() {
+    //decide whos turn it is
+    //get user input
+}

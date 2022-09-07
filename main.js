7a7e63b2fb4dd2eaf35b0ba87536fee308a4ec57
Daim <damiangoodenough1@gmail.com>
2022-09-06 21:12:36 -0700

    Make reset button work, still need to work on styling and themes.

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
 $('document').ready(() => {
   const $dropdown = $('.dropdown')
   const $menuItem = $('.menu-item');
+  const $menuItemContainer = $('.menu-item-container')
  
 $menuItem.on('mouseenter', event => {
     $(event.currentTarget).next().show();
 })
-$dropdown.on('mouseleave', () => {
+$menuItemContainer.on('mouseleave', () => {
     $dropdown.hide();
 })
+
+
+//themese
+function changeTheme(body, header, text, border) {
+ const $body = $('body');
+ const $header = $('header');
+ const $all = $('*');
+ const $border1 = $('.border-bottom-right');
+ const $border2 = $('.border-bottom');
+ const $border3 = $('.border-right');
+ const $reset = $('.reset')
+
+$body.css('background-color', body);
+$header.css('background-color', header);
+$all.css('color', text);
+$border1.css({
+              "border-bottom": "2px solid " + border,
+              "border-right": "2px solid " + border
+              });
+$border2.css("border-bottom", "2px solid " + border)
+$border3.css("border-right", "2px solid " + border)
+$reset.css("background-color", header)
+
+
+}
+
+$dark = $('#dark');
+$bitcoin = $('#bitcoin');
+$noTheme = $('#no-theme');
+
+$dark.on('click', changeTheme("darkgrey", "black", "white", "black"));
+
+//$bitcoin.on('click', changeTheme("orange", "black", "white", "white"))
+  
+
 
 
 })
--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,4 @@
-//declare variable that represent the spaces, 
+//declare variable that represent the spaces,
 //the board and winning positions
 let $box1;
 let $box2;
@@ -10,40 +10,37 @@ let $box7;
 let $box8;
 let $box9;
 let boardArray = [];
-let winners = []
+let winners = [];
 
 //function to reset variables to new values
 //every time a space is clicked;
-function setBoxes(){
-$box1 = $('#box1').children().text();
-$box2 = $('#box2').text();
-$box3 = $('#box3').text();
-$box4 = $('#box4').text();
-$box5 = $('#box5').text();
-$box6 = $('#box6').text();
-$box7 = $('#box7').text();
-$box8 = $('#box8').text();
-$box9 = $('#box9').text();
+function setBoxes() {
+  $box1 = $("#box1").children().text();
+  $box2 = $("#box2").text();
+  $box3 = $("#box3").text();
+  $box4 = $("#box4").text();
+  $box5 = $("#box5").text();
+  $box6 = $("#box6").text();
+  $box7 = $("#box7").text();
+  $box8 = $("#box8").text();
+  $box9 = $("#box9").text();
 
-boardArray = 
-[
-  [$box1, $box2, $box3],
-  [$box4, $box5, $box6],
-  [$box7, $box8, $box9]
-];
+  boardArray = [
+    [$box1, $box2, $box3],
+    [$box4, $box5, $box6],
+    [$box7, $box8, $box9],
+  ];
 
-winners =
-[
-  [boardArray[0][0], boardArray[0][1], boardArray[0][2]], //1st row
-  [boardArray[1][0], boardArray[1][1], boardArray[1][2]], //2nd row
-  [boardArray[2][0], boardArray[2][1], boardArray[2][2]], //3rd row
-  [boardArray[0][0], boardArray[1][0], boardArray[2][0]], //1st column
-  [boardArray[0][1], boardArray[1][1], boardArray[2][1]], //2nd column
-  [boardArray[0][2], boardArray[1][2], boardArray[2][2]], //3rd column
-  [boardArray[0][0], boardArray[1][1], boardArray[2][2]], //diagonal 1
-  [boardArray[0][2], boardArray[1][1], boardArray[2][0]] //diagonal 2
-
-];
+  winners = [
+    [boardArray[0][0], boardArray[0][1], boardArray[0][2]], //1st row
+    [boardArray[1][0], boardArray[1][1], boardArray[1][2]], //2nd row
+    [boardArray[2][0], boardArray[2][1], boardArray[2][2]], //3rd row
+    [boardArray[0][0], boardArray[1][0], boardArray[2][0]], //1st column
+    [boardArray[0][1], boardArray[1][1], boardArray[2][1]], //2nd column
+    [boardArray[0][2], boardArray[1][2], boardArray[2][2]], //3rd column
+    [boardArray[0][0], boardArray[1][1], boardArray[2][2]], //diagonal 1
+    [boardArray[0][2], boardArray[1][1], boardArray[2][0]], //diagonal 2
+  ];
 }
 
 //determines if there is a cats game or 3 in a row
@@ -51,46 +48,67 @@ function isGameOver(event) {
   setBoxes();
 
   //cats game
-  let board = [...$('.box').children().text()]     //destructure jQuery object of '.player-symbol' into board array
-  if(board.length === 9) console.log("Cats game"); //if board array is full its a cats game
-  
+  let board = [...$(".box").children().text()]; //destructure jQuery object of '.player-symbol' into board array
+  if (board.length === 9) {
+    let endGame1 = $("<div class = 'end-game'></div>").text("Cats Game!");
+    let endGame2 = $("<div class = 'end-game'></div>").text(
+      "There are no winners, please play again."
+    );
+    $(".gameboard").hide();
+    $(".end-game-container").append(endGame1, endGame2);
+    $(".end-game-container").show()
+
+    $(".reset").show();
+  } //if board array is full its a cats game
+
   //winner
-  const won = winners.some(arr => {     //if one of the array in 'winners' has same symbols that symbol has won
-    return arr.every(symbol => {
+  const won = winners.some((arr) => {
+    //if one of the array in 'winners' has same symbols that symbol has won
+    return arr.every((symbol) => {
       return symbol === currentPlayer;
-    })
-  })
-  if(won) console.log(currentPlayer + " won")
+    });
+  });
+  if (won) {
+    let endGame1 = $("<div class = 'end-game'></div>").text(`${currentPlayer} Won!`)
+    let endGame2 = $("<div class = 'end-game'></div>").text(
+      "Play again?"
+      );
+    $(".gameboard").hide();
+    $(".end-game-container").append(endGame1, endGame2);
+    $(".end-game-container").show()
+    $(".reset").show();
+
+  }
 }
 
-let currentPlayer = 'O';
+let currentPlayer = "O";
 
 //every time space is clicked this is called
 //depending on current player it sets a new div element of class "player symbol"
 // to the inner html of that box
 function updateBoard(event) {
-    if (currentPlayer === "X") {
-        const x = document.createElement("div");
-        x.innerHTML = "X";
-        x.classList.add("player-symbol");
-        event.currentTarget.append(x);
-        isGameOver(event);
-        currentPlayer = "O";
-      } else {
-        const o = document.createElement("div");
-        o.innerHTML = "O";
-        o.classList.add("player-symbol");
-        event.currentTarget.append(o);
-        isGameOver(event);
-        currentPlayer = "X";
-      }
+  if (currentPlayer === "X") {
+    const x = document.createElement("div");
+    x.innerHTML = "X";
+    x.classList.add("player-symbol");
+    event.currentTarget.append(x);
+    isGameOver(event);
+    currentPlayer = "O";
+  } else {
+    const o = document.createElement("div");
+    o.innerHTML = "O";
+    o.classList.add("player-symbol");
+    event.currentTarget.append(o);
+    isGameOver(event);
+    currentPlayer = "X";
+  }
 }
 
 //accepts an event
 //determines whether or not space is already taken
 function isMoveValid(event) {
   const $space = $(event.currentTarget);
-  if ($space.text() === '') {
+  if ($space.text() === "") {
     return true;
   } else {
     return false;
@@ -100,13 +118,15 @@ function isMoveValid(event) {
 //if isMoveValid returns false, then change the
 //background color to red while the mouse is clicked
 //and back to original when unclicked
-$(".box").on("mousedown", (event) => {
-  if(!isMoveValid(event)){
-  $(event.currentTarget).css("background-color", "red");
-  }
-}).on('mouseup', (event) => {
-  $(event.currentTarget).css("background-color", "")
-})
+$(".box")
+  .on("mousedown", (event) => {
+    if (!isMoveValid(event)) {
+      $(event.currentTarget).css("background-color", "red");
+    }
+  })
+  .on("mouseup", (event) => {
+    $(event.currentTarget).css("background-color", "");
+  });
 
 /*
 1.check if move is valid
@@ -117,10 +137,28 @@ $(".box").on("mousedown", (event) => {
     -if over pop up winning(or draw) message and option to reset
     -if not over exit function
 */
-$(".box").on("click", (event) => {
-  if(!isMoveValid(event)) {
+function tictactoe() {$(".box").on("click", (event) => {
+  if (!isMoveValid(event)) {
     return;
   }
   updateBoard(event);
 });
+}
 
+tictactoe();
+
+const $reset = $('.reset')
+
+//clear game board and start new game when reset button is clicked
+$reset.on('click', () => {
+  const $gameboard = $('.gameboard')
+  const $endGame = $('.end-game-container')
+  const $symbolDiv = $('.player-symbol')
+  const $endGameContainer = $('.end-game-container');
+  $reset.hide();
+  $endGame.hide();
+  $symbolDiv.remove();
+  $gameboard.show();
+  $endGameContainer.html("");
+  tictactoe();
+})

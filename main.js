91f631437d74624374cc832421f489d21776ed19
unknown <dgoodenough@proton.me>
2022-09-08 12:30:27 -0700

    Make themes work and add the about page. Need to style the about page, then I'm done!!

--- main.js
+++ main.js
@@ -12,9 +12,10 @@ $menuItemContainer.on('mouseleave', () => {
 
 
 //themese
-function changeTheme(body, header, text, border) {
+function changeTheme(body, header, text, border, backgroundImage = 'none') {
  const $body = $('body');
  const $header = $('header');
+ const $dropdownBackground = $('.dropdown')
  const $all = $('*');
  const $border1 = $('.border-bottom-right');
  const $border2 = $('.border-bottom');
@@ -22,14 +23,17 @@ function changeTheme(body, header, text, border) {
  const $reset = $('.reset')
 
 $body.css('background-color', body);
+$body.css('background-image', backgroundImage)
+$body.css('background-size', '50px')
 $header.css('background-color', header);
+$dropdownBackground.css('background-color', header);
 $all.css('color', text);
 $border1.css({
-              "border-bottom": "2px solid " + border,
-              "border-right": "2px solid " + border
+              "border-bottom": "4px solid " + border,
+              "border-right": "4px solid " + border
               });
-$border2.css("border-bottom", "2px solid " + border)
-$border3.css("border-right", "2px solid " + border)
+$border2.css("border-bottom", "4px solid " + border)
+$border3.css("border-right", "4px solid " + border)
 $reset.css("background-color", header)
 
 
@@ -39,8 +43,17 @@ $dark = $('#dark');
 $bitcoin = $('#bitcoin');
 $noTheme = $('#no-theme');
 
-// $dark.on('click', changeTheme("darkgrey", "black", "white", "black"));
+$dark.on('click', function(body, header, text, border) {
+  changeTheme("#39393d", "black", "white", "white");
+});
 
+$bitcoin.on('click', function(body, header, text, border, background) {
+  changeTheme("#f2a90070", "lightgrey", "black", "black",  'url(https://pngimg.com/uploads/bitcoin/bitcoin_PNG38.png)');
+});
+
+$noTheme.on('click', function(body, header, text, border) {
+  changeTheme("", "", "", "");
+})
 //$bitcoin.on('click', changeTheme("orange", "black", "white", "white"))
   
 
